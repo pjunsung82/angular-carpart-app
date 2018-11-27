9fb8a8d17dffaf62d7de8345335001228dc601f9
pjunsung82 <pjunsung82@gmail.com>
2018-11-27 21:45:33 +0900

    test

---
+++ src/app/carparts/car-part.ts
@@ -0,0 +1,10 @@
+export class CarPart { // 모델 클래스, 클래스 타입
+  id: number;
+  name: string;
+  description: string;
+  inStock: number;
+  price: number;
+  image: string;
+  quantity: number;
+  featured: boolean;
+}
