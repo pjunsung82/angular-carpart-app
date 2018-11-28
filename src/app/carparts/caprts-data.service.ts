3203eb74a3123dd78327f2abd981617d4fafc87f
pjunsung82 <pjunsung82@gmail.com>
2018-11-28 21:49:16 +0900

    test

--- src/app/carparts/caprts-data.service.ts
+++ src/app/carparts/caprts-data.service.ts
@@ -1,21 +1,31 @@
 import {Inject, Injectable} from '@angular/core';
 import {CarPart} from './car-part';
+import {HttpClient} from '@angular/common/http';
+
 import {CARPARTS} from './mocks';
+import {Observable} from 'rxjs';
 
 //@Injectable({
-//   providedIn : 'root'
+//   providedIn : 'root' // 6버전 부터
 // })
 // app.module.ts에
 // providers: [CarpartsDataService], 안해줘도 된다.
+// 5버전까지 이렇게 했다.
 @Injectable({
   providedIn : 'root'
 })
 export class CarpartsDataService {
-  constructor() {
+  // constructor() {
+  constructor(private httpClient: HttpClient) {
     console.log('CarpartsDataService consturctor called..');
   }
 
-  getCarParts() {
+/*  getCarParts() {
     return CARPARTS; // CARPARTS 상수
+  }*/
+
+  getCarParts(): Observable<any> {
+    return this.httpClient.get('assets/car-part.json',
+      {responseType: 'json'});
   }
 }
